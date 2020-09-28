ec42c5f748f40c26f4c69be98661d016a95f65c2
Muhammad Sidik <pasker.shinigami@gmail.com>
2020-09-28 21:31:13 +0700

    edit many to many table

--- database/migrations/2020_09_28_093428_category_post.php
+++ database/migrations/2020_09_28_093428_category_post.php
@@ -4,9 +4,15 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CategoryPost extends Migration{
-
-    public function up(){
+class CategoryPost extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
         Schema::create('category_post', function (Blueprint $table) {
             $table->foreignId("post_id")->constrained('post');
             $table->foreignId("category_id")->constrained('category');
@@ -14,7 +20,13 @@ class CategoryPost extends Migration{
         });
     }
 
-    public function down(){
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
         Schema::dropIfExists('category_post');
     }
 }
